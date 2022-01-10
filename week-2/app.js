1b84ffda661365cf5375ffa5bb902c0d3c2c8074
Weil_燃燒那不大的大腦 <74590513+f0955710119@users.noreply.github.com>
2022-01-10 16:16:50 +0800

    test assignment 1 ~ 3

---
+++ week-2/app.js
@@ -0,0 +1,8 @@
+const max = require('./assignment-1.js');
+const { calculate, calculateAnother } = require('./assignment-2.js');
+const { dataAssignment, avg } = require('./assignment-3.js');
+
+max([1, 4, 6, 2]);
+calculate({ op: '+', n1: 1, n2: 2 });
+calculateAnother({ op: '+', n1: 1, n2: 2 });
+avg(dataAssignment);
