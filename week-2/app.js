e0418b3779672c74385dec9c25cfdbe86a9f59d5
Weil_燃燒那不大的大腦 <74590513+f0955710119@users.noreply.github.com>
2022-01-10 17:13:26 +0800

    add click functionality to change hero welcome text and check nav menu

--- week-2/app.js
+++ week-2/app.js
@@ -1,3 +1,26 @@
+/*
+-----------SECTION DOM & EVENT -----------
+*/
+const heroBlock = document.querySelector('.hero');
+const welcomeMessage = document.querySelector('.primary-text');
+
+heroBlock.addEventListener(
+  'click',
+  () => (welcomeMessage.textContent = 'Have a Good Time!')
+);
+
+const nav = document.querySelector('.nav');
+const iconMenuOpen = document.querySelector('.icon--menu--open');
+const iconMenuClose = document.querySelector('.icon--menu--close');
+const navList = document.querySelector('.nav__list');
+iconMenuOpen.addEventListener('click', () => nav.classList.add('open-menu'));
+iconMenuClose.addEventListener('click', () =>
+  nav.classList.remove('open-menu')
+);
+
+/*
+-----------SECTION tesing -----------
+*/
 const max = require('./assignment-1.js');
 const { calculate, calculateAnother } = require('./assignment-2.js');
 const { dataAssignment, avg } = require('./assignment-3.js');
