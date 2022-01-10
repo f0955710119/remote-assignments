51d2688c407a448e2c3d3ba999093ca2a188d45c
Weil_燃燒那不大的大腦 <74590513+f0955710119@users.noreply.github.com>
2022-01-10 21:49:57 +0800

    split test code and DOM manipulating code

--- week-2/app.js
+++ week-2/app.js
@@ -30,14 +30,3 @@ buttonCTA.addEventListener('click', () => {
   ctaClicked = !ctaClicked;
   productContainerSub.style.display = 'grid';
 });
-/*
------------SECTION tesing -----------
-*/
-const max = require('./assignment-1.js');
-const { calculate, calculateAnother } = require('./assignment-2.js');
-const { dataAssignment, avg } = require('./assignment-3.js');
-
-max([1, 4, 6, 2]);
-calculate({ op: '+', n1: 1, n2: 2 });
-calculateAnother({ op: '+', n1: 1, n2: 2 });
-avg(dataAssignment);
