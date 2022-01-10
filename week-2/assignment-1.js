c3b9cc3390b0487fc95b6e19b33ebb65332ac3be
Weil_燃燒那不大的大腦 <74590513+f0955710119@users.noreply.github.com>
2022-01-10 15:45:15 +0800

    create a function to find out the max number in an array

---
+++ week-2/assignment-1.js
@@ -0,0 +1,30 @@
+/* 
+  Success (be careful to the statement of for lop) 
+  https://developer.mozilla.org/zh-TW/docs/Web/JavaScript/Guide/Loops_and_iteration#for...in_%E9%99%B3%E8%BF%B0%E5%BC%8F
+*/
+exports.module = function max(numbers) {
+  let max = 0;
+
+  for (const number of numbers) {
+    if (number > max) {
+      max = number;
+    }
+  }
+
+  console.log(`The max number is ${max}.`);
+  return max;
+};
+
+// Practice: The max number is 3.
+// function max(numbers) {
+//   let max = 0;
+
+//   for (const number in numbers) {
+//     if (number > max) {
+//       max = number;
+//     }
+//   }
+
+//   console.log(`The max number is ${max}.`);
+//   return max;
+// }
