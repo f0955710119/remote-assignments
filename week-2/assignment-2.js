1536b07b911d451a6660245e55dff59cdf9d12f3
Weil_燃燒那不大的大腦 <74590513+f0955710119@users.noreply.github.com>
2022-01-18 23:37:47 +0800

    add two ways to create objects ( assignment 2 )

--- week-2/assignment-2.js
+++ week-2/assignment-2.js
@@ -1,4 +1,4 @@
-function calculate({ ...args }) {
+function calculate(args) {
   let result;
   if (args.op === '+') {
     result = args.n1 + args.n2;
@@ -10,21 +10,25 @@ function calculate({ ...args }) {
   console.log(`The result of this operation is ${result}.`);
   return result;
 }
-
-function calculateAnother(args = { op, n1, n2 }) {
-  let result;
-  if (args.op === '+') {
-    result = args.n1 + args.n2;
-  } else if (args.op === '-') {
-    result = args.n1 - args.n2;
-  } else {
-    result = 'Not supported';
+// class sugar syntax
+class CalcObj1 {
+  n1 = 1;
+  n2 = 2;
+  constructor(op) {
+    this.op = op;
   }
-  console.log(`The result of this operation is ${result}.`);
-  return result;
+}
+// function return object
+function calcObj2(op) {
+  return {
+    op,
+    n1: 1,
+    n2: 2,
+  };
 }
 
 module.exports = {
   calculate,
-  calculateAnother,
+  CalcObj1,
+  calcObj2,
 };
--- week-2/assignment-test.js
+++ week-2/assignment-test.js
@@ -2,12 +2,21 @@
 -----------SECTION tesing -----------
 */
 const max = require('./assignment-1.js');
-const { calculate, calculateAnother } = require('./assignment-2.js');
+const { calculate, CalcObj1, calcObj2 } = require('./assignment-2.js');
 const { dataAssignment, avg } = require('./assignment-3.js');
 const { twoSum } = require('./assignment-advanced-algo');
 
+// assignment 1: create a max()
 max([1, 4, 6, 2]);
-calculate({ op: '+', n1: 1, n2: 2 });
-calculateAnother({ op: '+', n1: 1, n2: 2 });
+
+// assignment 2: create at least two objects
+const object1 = new CalcObj1('+');
+calculate(object1);
+const object2 = calcObj2('-');
+calculate(object2);
+
+// assignment 3: show the average price of all products
 avg(dataAssignment);
+
+// assignment advanced: sum algo
 twoSum([1, 2, 3, 4, 1, 4], 5);
